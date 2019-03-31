892a10ad444a9f144e736b82ec1d2f9802282e57
victorighalo <victorighalo@live.com>
2019-03-31 14:08:03 +0100

    Renamed App, Add Draw navigator

--- android/app/src/main/java/com/PickUp/MainActivity.java
+++ android/app/src/main/java/com/PickUp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "t37";
+        return "PickUp";
     }
 
       @Override
--- src/layout/AppContainer.js
+++ src/layout/AppContainer.js
@@ -1,26 +1,13 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { createStackNavigator, createAppContainer,createSwitchNavigator } from "react-navigation";
-import Icon from 'react-native-vector-icons/FontAwesome';
-import ActionButton from './actionButtons'
-import {Footer, Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Text } from 'native-base';
+import {createStackNavigator, createAppContainer, createSwitchNavigator, createDrawerNavigator} from "react-navigation";
+import {Icon} from 'react-native-elements'
 import ProfileScreen from '../screens/profile'
 import LoginScreen from '../screens/LoginScreen'
 import RegisterScreen from '../screens/RegisterScreen'
 import PeopleScreen from "../screens/PeopleScreen";
 
-class AboutScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-          <Text>About Screen</Text>
-        </View>
-      );
-    }
-  }
-
-
 const FirstStack = createStackNavigator({
+
         People: {
             screen: PeopleScreen
         },
@@ -30,25 +17,31 @@ const FirstStack = createStackNavigator({
     },
     {
         initialRouteName: 'People',
-        defaultNavigationOptions: ({ navigation }) => ({
-            header: null
-            // tabBarIcon: ({ focused, horizontal, tintColor }) => {
-            //   const { routeName } = navigation.state;
-            // //   let IconComponent = Ionicons;
-            //   let iconName;
-            //   if (routeName === 'Home') {
-            //     iconName = `home`;
-            //     // Sometimes we want to add badges to some icons.
-            //     // You can check the implementation below.
-            //     // IconComponent = HomeIconWithBadge;
-            //   } else if (routeName === 'About') {
-            //     iconName = `info`;
-            //   }
+        defaultNavigationOptions: ({ navigation }) => {
+            return ({
+                drawerLabel: 'Home',
+                headerLeft: ({tintColor}) => (
+                    <Icon name="menu" onPress={()=> navigation.openDrawer()} size={30} style={{paddingLeft: 20}}/>
+                ),
+                // header: null
+                // tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                //   const { routeName } = navigation.state;
+                // //   let IconComponent = Ionicons;
+                //   let iconName;
+                //   if (routeName === 'Home') {
+                //     iconName = `home`;
+                //     // Sometimes we want to add badges to some icons.
+                //     // You can check the implementation below.
+                //     // IconComponent = HomeIconWithBadge;
+                //   } else if (routeName === 'About') {
+                //     iconName = `info`;
+                //   }
 
-            //   // You can return any component that you like here!
-            //   return <Icon name={iconName} size={25} color={tintColor} />;
-            // }
-        })
+                //   // You can return any component that you like here!
+                //   return <Icon name={iconName} size={25} color={tintColor} />;
+                // }
+            });
+        }
     }
 );
 
@@ -67,11 +60,17 @@ const AuthStack = createStackNavigator({
     }
 );
 
+const AppDrawerNavigator = createDrawerNavigator({
+    Home: {
+        screen: FirstStack,
+    }
+});
+
   const root = createSwitchNavigator({
       Auth: {
           screen: AuthStack
       },
-      Stack: FirstStack
+      Stack: AppDrawerNavigator
   })
 
   
